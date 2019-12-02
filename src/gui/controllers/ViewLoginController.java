6665a24f1a2f771a2ec7f888ad2ffd4b7846de7a
Leonardo Petta <leonardocps9@gmail.com>
2019-12-02 14:49:16 -0300

    Implementação funcional DaoLogin
    Daologin está integrado ao padrão MVC

--- src/gui/controllers/ViewLoginController.java
+++ src/gui/controllers/ViewLoginController.java
@@ -1,23 +1,16 @@
 package gui.controllers;
 
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-
-import javax.swing.JOptionPane;
 
 import application.Main;
-import db.DB;
-import gui.util.Alerts;
 import gui.util.Notificacoes;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import model.dao.DaoLogin;
 import model.entities.Login;
-import model.exceptions.DbException;
 
 public class ViewLoginController {
 	static Statement st = null;
@@ -38,46 +31,14 @@ public class ViewLoginController {
 		String useTemp = tfUsuario.getText();
 		String passTemp = pfSenha.getText();
 		Login login = new Login(useTemp, passTemp);
-		if(carregaLogin(login) == false && tentativas >4) {
-			Main.getStage().close();
+		if(useTemp.isEmpty() || passTemp.isEmpty()) {
+			Notificacoes.mostraNotificacao("Aviso!", "Há campos vazios, preencha usuário ou senha!");
 		}
-	}
-	
-	public boolean carregaLogin(Login login) {
-		try {	
-			 st = DB.getConnection().createStatement();
-			 rs = st.executeQuery("select * from usuario");
-			 ArrayList<Login> arrayLogin = new ArrayList<>();
-			 while(rs.next()) {
-				 String usuario = rs.getString("login");
-				 String senha = rs.getString("senha");
-				 Login loginTemp = new Login(usuario, senha);
-				 arrayLogin.add(loginTemp);
-			 }
-			 if(arrayLogin.contains(login)) {
-				Main.getStage().setScene(Main.getMain());
-				Main.getStage().centerOnScreen();
-				arrayLogin.clear();;
-				return true;
+		else {
+			if(DaoLogin.carregaLogin(login, tfUsuario, pfSenha) == false && tentativas > 4) {
+				Main.getStage().close();
 			}
-			else {
-				Notificacoes.mostraNotificacao("Erro de login!!", "Usuário ou senha incorreto\nEssa é a tentativa: "+(tentativas)+" de 3");
-				tfUsuario.clear();
-				pfSenha.clear();
-				tentativas += 1;
-				return false;
-			}
-		}
-		catch(SQLException e) {
-			JOptionPane.showMessageDialog(null, e.getMessage());
-			System.exit(0);
-		}
-		finally {
-			DB.closeConnection();
-			DB.fechaResultSet(rs);
-			DB.fechaStatement(st);
-		}
-		return false;
+		}	
 	}
 
 	public static int getTentativas() {
